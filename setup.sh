0f739fbab462ada15a4e3cade52253c29b3d8085
SKi <megamanics@users.noreply.github.com>
2023-05-06 17:39:33 -0700

    rrmwrkflw

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-repo=$(git remote get-url origin | cut -d ":" -f 2 | cut -d "/" -f 1-2)
-issue=$(echo audit | gh issue create -l bug -a "megamanics" -t "[Audit]" -R ${}repo} -F -)
+repo=$(git remote get-url origin | sed 's/.*\/\([^ ]*\/[^ ]*\)\.git/\1/')
+issue=$(echo audit | gh issue create -l bug -a "megamanics" -t "[Audit]" -R ${repo} -F -)
 echo $issue
 issueno=$(echo $issue | cut -f7 -d/)
 branch=branch-4-issue-${issueno}
@@ -7,8 +7,9 @@ gh issue edit ${issueno} -t "[Audit] ${issueno}"
 gh issue comment  ${issueno} --body ":octocat:"
 gh api /repos/${repo}/issues/${issueno}/reactions -f content='heart' --silent
 git checkout -b ${branch}
-echo "#${issueno}" >> setup.sh
-git commit -m "update setup for #${issueno}" setup.sh
+echo "#${issueno}" > edit.txt
+git add edit.txt
+git commit -m "update setup for #${issueno}" edit.txt
 git push   --set-upstream origin  ${branch}
 git branch --set-upstream-to=origin/${branch}  ${branch}
 git pull   --rebase --autostash --all --ff
@@ -16,7 +17,3 @@ git push   --set-upstream origin  ${branch}
 git push
 gh pr create -a "@me" -b "close #${issueno}" -B main -f
 gh pr merge  -b "merged"     -r -d --auto
-#
-#
-#
-#
